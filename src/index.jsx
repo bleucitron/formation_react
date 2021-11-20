4af7be608d2eabac70c1ee8f9e216c123373e640
Romain Crestey <rcrestey@gmail.com>
2021-11-20 19:19:52 +0100

    update deps

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,4 @@ import './index.css';
 import data from './_data/pokemon.json';
 import App from './classes/App';
 
-import './index.scss';
-
 ReactDOM.render(<App data={data} />, document.getElementById('root'));
