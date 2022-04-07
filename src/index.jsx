79e88863a606447f5d4210d07931a2d4782b8130
Romain Crestey <rcrestey@gmail.com>
2022-04-07 12:03:37 +0200

    2022-04-07

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Pokemon from './classes/Pokemon';
+
 import './reset.css';
 import './index.css';
 
-const pokemon = <div>Pikachu</div>;
+const app = (
+  <>
+    <ul>
+      <Pokemon name="Pikachu" weight={6} />
+      <Pokemon name="Bulbizarre" weight={12} />
+    </ul>
+    <div className="Trainer">
+      <div className="name">Romain</div>
+      <div className="address">1 rue des pokemons</div>
+    </div>
+  </>
+);
 
-ReactDOM.render(pokemon, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
